efa447b5857a367ca140be4529bffe802ddd2a94
Barbara Verburg <barb@zenzillo.com>
2017-11-03 12:51:50 -0400

    Show infowindow when clicked

--- js/map.js
+++ js/map.js
@@ -43,6 +43,23 @@ function createMapMarkers(locations) {
 }
 
 
+// This function populates the infowindow when the marker is clicked. We'll only allow
+// one infowindow which will open at the marker that is clicked, and populate based
+// on that markers position.
+function populateInfoWindow(marker, infowindow) {
+	// Check to make sure the infowindow is not already opened on this marker.
+	if (infowindow.marker != marker) {
+	  infowindow.marker = marker;
+	  infowindow.setContent('<div>' + marker.title + '</div>');
+	  infowindow.open(map, marker);
+	  // Make sure the marker property is cleared if the infowindow is closed.
+	  infowindow.addListener('closeclick', function() {
+	    infowindow.marker = null;
+	  });
+	}
+
+}
+
 // This function will loop through the markers array and display them all.
 function showListings() {
 	var bounds = new google.maps.LatLngBounds();
